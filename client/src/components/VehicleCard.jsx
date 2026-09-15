05467b417e4e8b7baba27d65d848c79b1e5ad1a0
Eliteon150ping <rishensurjoobhalee@gmail.com>
2026-09-15 15:55:16 +0200

    My garage Page fully styled, going to do the add car form next

--- client/src/components/VehicleCard.jsx
+++ client/src/components/VehicleCard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import ConfirmationModal from "./ConfirmationModal";
 import "../styles/VehicleCard.css"
@@ -6,29 +6,61 @@ import "../styles/VehicleCard.css"
 function VehicleCard({ id, brand, model, year, currentMileage, onEdit, onDelete }) {
 
     const [showConfirmationCard, setShowConfirmationCard] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
+
+    useEffect(() => {
+
+        function handleClickOutside(event) {
+            if (!event.target.closest(".vehicle-menu")) {
+                setShowMenu(false);
+            }
+        }
+        document.addEventListener("click", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("click", handleClickOutside);
+        };
+    }, []);
 
     return (
-        <div className="main-vehicle-card">
-            <Link to={`/vehicles/${id}`}>
-                <div className="vehicle-card">
-                    <img className="vehicle-image" src="/images/generic_car.png" alt="Generic car" />
-                    <h3>{year} {brand} {model}</h3>
-                    <p>Mileage: {currentMileage.toLocaleString()} km</p>
-                </div>
-            </Link>
-            <button type="edit" onClick={onEdit}>Edit</button>
-            <button onClick={() => setShowConfirmationCard(true)}>Delete</button>
+        <div>
+            <div className="main-vehicle-card">
+                <Link className="vehicle-link" to={`/vehicles/${id}`}>
+                    <div className="vehicle-card">
+                        <img className="vehicle-image" src="/images/generic_car.png" alt="Generic car" />
+                        <h3 className="vehicle-name">{year} {brand} {model}</h3>
+                        <p className="vehicle-mileage">Mileage: {currentMileage.toLocaleString()} km</p>
+                    </div>
+                </Link>
 
+                <div className="vehicle-menu">
+                    <button className="vehicle-menu-button"
+                        onClick={() => setShowMenu(!showMenu)}
+                    >⋮</button>
+
+                    {showMenu && (
+                        <div className="vehicle-menu-options">
+                            <button onClick={onEdit}>Edit</button>
+                            <button className="delete-button" onClick={() => {
+                                setShowMenu(false);
+                                setShowConfirmationCard(true);
+                            }}>Delete</button>
+                        </div>
+                    )}
+
+                </div>
+            </div>
+            
             {showConfirmationCard && (
-                <ConfirmationModal
-                    title="Delete Car"
-                    message="Sold your car? You can delete it from your garage"
-                    confirmText="Delete Car"
-                    cancelText="Cancel"
-                    onConfirm={onDelete}
-                    onCancel={() => setShowConfirmationCard(false)}
-                />
-            )}
+                    <ConfirmationModal
+                        title="Delete Car"
+                        message="Sold your car? You can delete it from your garage"
+                        confirmText="Delete Car"
+                        cancelText="Cancel"
+                        onConfirm={onDelete}
+                        onCancel={() => setShowConfirmationCard(false)}
+                    />
+                )}
         </div>
     );
 }
